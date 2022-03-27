4562345841cbc75ad649812ff85b89935aa10551
Thorinwasher <ragnarlol96@gmail.com>
2022-03-27 21:14:50 +0200

    Some setup

---
+++ src/main/java/dev/thorinwasher/langlint/LanguageLinter.java
@@ -0,0 +1,31 @@
+package dev.thorinwasher.langlint;
+
+import java.util.Locale;
+
+/**
+ * 
+ * @author Thorin
+ * @author Pheotis
+ *
+ */
+public class LanguageLinter {
+    
+    /**
+     * Filter the text allowed in the specified language
+     * @param locale    <p> The iso-639-1 language to filter</p>
+     * @param text      <p> The text to filter </p>
+     * @return
+     */
+    static public String filterText(Locale locale, String text) {
+        return null;
+    }
+    
+    /**
+     * Filter the text allowed in any language
+     * @param text  <p> The text to filter </p>
+     * @return
+     */
+    static public String filterText(String text) {
+        return null;
+    }
+}
